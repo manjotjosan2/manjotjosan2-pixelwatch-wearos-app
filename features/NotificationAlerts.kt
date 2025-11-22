d46644d6c5a68dacce1fb1dde3124f9f2815cbe3
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 14:40:43 -0800

    Create NotificationAlerts.kt

---
+++ features/NotificationAlerts.kt
@@ -0,0 +1,7 @@
+package com.pixelwatch.features
+
+class NotificationAlerts {
+    fun triggerAlert(alertType: String) {
+        // Handle triggering of notification or alert on the watch
+    }
+}
