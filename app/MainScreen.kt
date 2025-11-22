4a5871b08b890f0c7bea67257ea1ea5b752a373d
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 14:38:33 -0800

    Implement MainScreen composable for Pixel Watch app

---
+++ app/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.pixelwatch.app
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Scaffold {
+        Text("Pixel Watch Wear OS App")
+    }
+}
