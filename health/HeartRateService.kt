fab66dd1c6498afd4129a4127bc2b5c92f2a09ab
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 14:40:06 -0800

    Add HeartRateService class for processing heart rate data

---
+++ health/HeartRateService.kt
@@ -0,0 +1,7 @@
+package com.pixelwatch.health
+
+class HeartRateService {
+    fun processHeartRateData(value: Int) {
+        // Handle heart rate sample (update dashboard, send to paired phone, etc)
+    }
+}
