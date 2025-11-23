7fecc37f6a9744a04d8dc9249a553c17dc8ffa6a
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 17:56:35 -0800

    Add syncHeartRate function to HeartRateService
    Added a syncHeartRate function to stream data.

--- health/HeartRateService.kt
+++ health/HeartRateService.kt
@@ -4,4 +4,7 @@ class HeartRateService {
     fun processHeartRateData(value: Int) {
         // Handle heart rate sample (update dashboard, send to paired phone, etc)
     }
+    fun syncHeartRate(value: Int) {
+    // Stream to paired phone using protocol format
+}
 }
